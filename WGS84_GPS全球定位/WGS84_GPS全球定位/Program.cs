5d7d8731da30c37e01efb0c541e34df9362881ea
刘晨 <7578916585@qq.com>
2018-01-03 22:08:14 +0800

    提交

--- WGS84_GPS全球定位/WGS84_GPS全球定位/Program.cs
+++ WGS84_GPS全球定位/WGS84_GPS全球定位/Program.cs
@@ -11,20 +11,21 @@ namespace WGS84_GPS全球定位
     {
         static void Main(string[] args)
         {
-            var a = SqlGeography.Point(22.54587746, 114.12873077, 4326); //上海的某个点
-            var b = SqlGeography.Point(23, 115, 4326); //上海的某个点,4236代表WGS84这种坐标参照系统。
-            Console.WriteLine(a.STDistance(b)); //距离
-
+            // 1.使用Microsoft.SqlServer.Types计算WGS84坐标的两点距离
             var c = SqlGeography.Point(31.837964, 117.203123, 4326); //公司的位置
             var d = SqlGeography.Point(31.822426, 117.221662, 4326); // 合肥市政务中心,4236代表WGS84这种坐标参照系统。
             Console.WriteLine("公司和合肥市政务中心的距离是：" + c.STDistance(d) + "米"); //距离
-            
+
+
+            // 2.使用把坐标当做平面坐标计算两点距离
             double width = 31.837964 - 31.822426;
             double height = 117.203123 - 117.221662;
             double result = (width * width) + (height * height);
             result = Math.Sqrt(result);//根号
             Console.WriteLine("想象成平面时公司和合肥市政务中心的距离是：" + result + "米"); //距离
 
+
+            // 3.用haversine公式计算WGS84坐标的两点距离
             WGS84 wgs84 = new WGS84();
             var coordinateA = new Coordinate() { Latitude = 31.837964, Longitude = 117.203123 };
             var coordinateB = new Coordinate() { Latitude = 31.822426, Longitude = 117.221662 };
