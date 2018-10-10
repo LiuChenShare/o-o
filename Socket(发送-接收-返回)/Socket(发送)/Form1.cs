7621327b4aa2684b6ad3faf0e168eee04ea43e5f
村长 <757891685@qq.com>
2018-10-10 13:59:52 +0800

    添加发送端

---
+++ Socket(发送-接收-返回)/Socket(发送)/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Socket_发送_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                return;
+            }
+            IPAddress ip = IPAddress.Parse("127.0.0.1");//接收端所在IP
+            IPEndPoint ipEnd = new IPEndPoint(ip, 1111);//接收端所监听的接口
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//初始化一个Socket对象
+            try
+            {
+                socket.Connect(ipEnd);//连接指定IP&端口
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("连接失败");
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+            var body = Encoding.UTF8.GetBytes(text);
+            //body = System.Text.Encoding.Default.GetBytes(ResultFilePath);
+            socket.Send(body);//发送数据
+
+            string str = System.Text.Encoding.Default.GetString(body);
+
+            //string filePath = @"C:\Users\admin\Desktop\log.txt";
+            //File.AppendAllText(filePath, Process.GetCurrentProcess().Id + "发送消息" + str + "\r\n");
+
+            while (true)//定义一个循环接收返回数据
+            {
+                byte[] data = new byte[1024];
+                var length = socket.Receive(data);//接收返回数据
+                string stringData = Encoding.UTF8.GetString(data, 0, length);
+                if (!string.IsNullOrWhiteSpace(stringData))
+                {
+                    Console.Write(stringData);
+                    MessageBox.Show("发送成功！");
+                    break;
+                }
+            }
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();//关闭Socket
+        }
+    }
+}
---
+++ Socket(发送-接收-返回)/Socket(接收)/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Socket_接收_
+{
+    public partial class Form1 : Form
+    {
+        Thread t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = new Thread(ReciveMsg);
+            t.Start();
+        }
+
+        /// <summary>        
+        /// /// 接收发送给本机ip对应端口号的数据报        
+        /// /// </summary>        
+        void ReciveMsg()
+        {
+            TcpListener tcpl = new TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), 1111);//定义一个TcpListener对象监听本地的1111端口
+            tcpl.Start();//监听开始
+            while (true)
+            {
+
+                Socket s = tcpl.AcceptSocket();//挂起一个Socket对象
+                string remote = s.RemoteEndPoint.ToString();//获取发送端的IP及端口转为String备用
+                Byte[] stream = new Byte[1024];
+                var length = s.Receive(stream);//接收发送端发过来的数据,写入字节数组
+                                               //BGW_Handle.ReportProgress(1, "接收来自[" + remote + "]信息");
+
+                //string str = System.Text.Encoding.Default.GetString(stream, 0, length);
+
+                string _data = System.Text.Encoding.UTF8.GetString(stream, 0, length);//将字节数据数组转为String
+                var result = _data;
+                s.Send(stream);//将接收到的内容，直接返回接收端
+                s.Shutdown(SocketShutdown.Both);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    //string filePath3 = @"C:\Users\admin\Desktop\log3.txt";
+                    //File.AppendAllText(filePath3, Process.GetCurrentProcess().Id + "接收到:" + _data + "\r\n");
+                    
+                    break;
+                }
+            }
+            tcpl.Stop();//停止监听
+        }
+    }
+}
