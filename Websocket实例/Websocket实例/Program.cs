6ba978676031e6d27705e36e5602505b7b979434
村长 <757891685@qq.com>
2018-10-12 14:12:47 +0800

    通过NuGet引入Fleck

--- Websocket实例/Websocket实例/Program.cs
+++ Websocket实例/Websocket实例/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fleck;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +17,40 @@ namespace Websocket实例
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://0.0.0.0:7181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
+
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+
             Application.Run(new Form1());
         }
     }
