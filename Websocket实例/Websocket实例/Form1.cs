b028c14e30384e0ca2c03407b1739a8d3691919e
村长 <757891685@qq.com>
2018-10-12 16:40:41 +0800

    完成WebSocket的示例

--- Websocket实例/Websocket实例/Form1.cs
+++ Websocket实例/Websocket实例/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fleck;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,56 @@ namespace Websocket实例
 {
     public partial class Form1 : Form
     {
+        private List<IWebSocketConnection> allSockets;
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            //FleckLog.Level = LogLevel.Debug;
+            allSockets = new List<IWebSocketConnection>();
+            //var server = new WebSocketServer("ws://localhost:12345");
+            var server = new WebSocketServer("ws://0.0.0.0:7181");
+            var a = server.Location;
+            var b = server.Port;
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    //Console.WriteLine("Open!");
+                    //MessageBox.Show("Open!");
+                    listBox1.Items.Add("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    //Console.WriteLine("Close!");
+                    //MessageBox.Show("Close!");
+                    listBox1.Items.Add("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    //Console.WriteLine(message);
+                    //MessageBox.Show(message);
+                    listBox1.Items.Add(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var input = textBox1.Text;
+            if (string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+            foreach (var socket in allSockets.ToList())
+            {
+                socket.Send(input);
+            }
         }
     }
 }
--- Websocket实例/Websocket实例/Program.cs
+++ Websocket实例/Websocket实例/Program.cs
@@ -17,40 +17,6 @@ namespace Websocket实例
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            FleckLog.Level = LogLevel.Debug;
-            var allSockets = new List<IWebSocketConnection>();
-            var server = new WebSocketServer("ws://0.0.0.0:7181");
-            server.Start(socket =>
-            {
-                socket.OnOpen = () =>
-                {
-                    Console.WriteLine("Open!");
-                    allSockets.Add(socket);
-                };
-                socket.OnClose = () =>
-                {
-                    Console.WriteLine("Close!");
-                    allSockets.Remove(socket);
-                };
-                socket.OnMessage = message =>
-                {
-                    Console.WriteLine(message);
-                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
-                };
-            });
-
-
-            var input = Console.ReadLine();
-            while (input != "exit")
-            {
-                foreach (var socket in allSockets.ToList())
-                {
-                    socket.Send(input);
-                }
-                input = Console.ReadLine();
-            }
-
             Application.Run(new Form1());
         }
     }
