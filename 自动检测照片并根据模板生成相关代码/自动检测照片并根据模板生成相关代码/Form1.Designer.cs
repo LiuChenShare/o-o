eb644f97be6c55d52dd8fdeb92fbd6594777abb8
村长 <757891685@qq.com>
2018-11-21 14:47:22 +0800

    完成小工具

--- 自动检测照片并根据模板生成相关代码/自动检测照片并根据模板生成相关代码/Form1.Designer.cs
+++ 自动检测照片并根据模板生成相关代码/自动检测照片并根据模板生成相关代码/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "共加载图片：";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.ForeColor = System.Drawing.Color.Red;
+            this.countLabel.Location = new System.Drawing.Point(96, 37);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(11, 12);
+            this.countLabel.TabIndex = 4;
+            this.countLabel.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "加载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(271, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(358, 88);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "照片小工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- 自动检测照片并根据模板生成相关代码/自动检测照片并根据模板生成相关代码/Form1.cs
+++ 自动检测照片并根据模板生成相关代码/自动检测照片并根据模板生成相关代码/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -15,5 +16,97 @@ namespace 自动检测照片并根据模板生成相关代码
         {
             InitializeComponent();
         }
+
+        public List<string> Images = new List<string>();
+        public string Template { get; set; }
+
+        /// <summary>
+        /// 加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.InitialDirectory = @"C:\Users\LWP1398\Desktop"; //设置初始路径
+            ofd.Filter = "Excel文件(*.xls)|*.xls|Csv文件(*.csv)|*.csv|所有文件(*.*)|*.*"; //设置“另存为文件类型”或“文件类型”框中出现的选择内容
+            ofd.Filter = "图片文件|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;*.tif;*.tiff";
+            //ofd.FilterIndex = 2; //设置默认显示文件类型为Csv文件(*.csv)|*.csv
+            ofd.Title = "选择图片"; //获取或设置文件对话框标题
+            ofd.RestoreDirectory = true;////设置对话框是否记忆上次打开的目录
+
+            ofd.Multiselect = true;//设置多选
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //for (int i = 0; i < ofd.FileNames.Length; i++)
+                //{
+                //    txtPath.Text += ofd.FileNames[i] + "\r\n";//输出一个路径回车换行
+                //}
+                //for (int i = 0; i < ofd.FileNames.Length; i++)
+                //{
+                //    txtPath.Text += ofd.SafeFileNames[i] + "\r\n";
+                //}
+                int count = ofd.FileNames.Length;
+                int sucess = 0;
+                for (int i = 0; i < ofd.FileNames.Length; i++)
+                {
+                    try
+                    {
+                        using (FileStream fs = new FileStream(ofd.FileNames[i], FileMode.Open, FileAccess.Read))
+                        {
+                            System.Drawing.Image image = System.Drawing.Image.FromStream(fs);
+                            double width = image.Width;
+                            double height = image.Height;
+                            double aspectRatio = width / height;
+                            var url = ofd.FileNames[i];
+
+                            //string fullPath = @"\WebSite1\Default.aspx";
+                            //string filename = System.IO.Path.GetFileName(fullPath);//文件名  “Default.aspx”
+                            //string extension = System.IO.Path.GetExtension(fullPath);//扩展名 “.aspx”
+                            //string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullPath);// 没有扩展名的文件名 “Default”
+
+                            var imageName = System.IO.Path.GetFileName(url);
+                            Images.Add(imageName);
+                            sucess++;
+                            Refresh(count, sucess);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                MessageBox.Show(string.Format("加载结束,总数{0}，加载{1}", count, sucess));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var templatePath = System.Environment.CurrentDirectory + @"\Template.txt";
+            Template = File.ReadAllText(templatePath);
+            var file = new SaveFileDialog();
+            file.Title = "选择生成路径";
+            file.Filter = "文本格式(*.txt)|*.txt";
+            file.FilterIndex = 1;
+            file.RestoreDirectory = true;
+            file.FileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            //先弹框，让用户选择
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string savePath = file.FileName;
+                foreach(var item in Images)
+                {
+                    var str = Template.Replace("{ImageName}", item);
+                    File.AppendAllText(savePath, str);
+                }
+            }
+            MessageBox.Show("生成结束");
+        }
+
+        public void Refresh(int count = 0, int sucess = 0)
+        {
+            this.countLabel.Text = Images.Count().ToString();
+        }
+
     }
 }
