22c96ad82269004b002112e9ff264f76826668ff
村长 <757891685@qq.com>
2019-07-08 19:10:44 +0800

    图片加水印

---
+++ 图片加水印/图片加水印/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace 图片加水印
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(239, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(192, 237);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "我是个水印";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "加水印";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 457);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 图片加水印/图片加水印/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 图片加水印
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 文件地址
+        /// </summary>
+        private string Path;
+        private Bitmap Bitmap1;
+        private Bitmap Bitmap2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog InvokeDialog = new OpenFileDialog();
+                InvokeDialog.Title = "选择文件";
+                //Filter = "图像文件|*.jpg;*.png;*.jpeg;*.bmp;*.gif|所有文件|*.*"
+                InvokeDialog.Filter = "图像文件|*.jpg;*.png;*.jpeg;*.bmp;*.gif";
+                InvokeDialog.RestoreDirectory = true;
+                if (InvokeDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    Path = InvokeDialog.FileName;
+                    if (File.Exists(Path))
+                    {
+                        Bitmap1 = new Bitmap(Path);
+                        pictureBox1.Image = Bitmap1;
+                    }
+                    else
+                    {
+                        MessageBox.Show(this, "请确认文件是否存在");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "选择失败:" + ex.Message);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var a = PngAddText(Path, textBox1.Text);
+            if (a != null)
+            {
+                pictureBox2.Image = a;
+            }
+        }
+
+
+        /// <summary>
+        /// 图片上嵌入文字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static Bitmap PngAddText(string imgPath, string text)
+        {
+            try
+            {
+                System.Drawing.Image imgSrc = System.Drawing.Image.FromFile(imgPath);
+                
+                float x = 0;
+                float y = 30;
+
+                float xOffset = 20;
+                using (Graphics g = Graphics.FromImage(imgSrc))
+                {
+                    g.DrawImage(imgSrc, 0, 0, imgSrc.Width, imgSrc.Height);
+                    using (Font f = new Font("楷体", 64, FontStyle.Bold))
+                    {
+                        using (Brush b = new SolidBrush(Color.FromArgb(160,205,205,180)))
+                        {
+                            //for (int i = 0; i < text.Length; i++)
+                            //{
+                            //    switch (i)
+                            //    {
+                            //        case 0:
+                            //            x = imgSrc.Width / 2 - xOffset;
+                            //            y = xOffset;
+                            //            break;
+                            //        case 1:
+                            //            x = imgSrc.Width / 2 - xOffset;
+                            //            y = imgSrc.Height / 2;
+                            //            break;
+                            //        case 2:
+                            //            if (text.Length == 4)
+                            //            {
+                            //                x = 0;
+                            //                y = xOffset;
+                            //            }
+                            //            else if (text.Length == 3)
+                            //            {
+                            //                x = 0;
+                            //                y = imgSrc.Height / 2 - 65 / 2.0f;
+                            //            }
+                            //            break;
+                            //        case 3:
+                            //            x = 0;
+                            //            y = imgSrc.Height / 2;
+                            //            break;
+                            //        default:
+                            //            break;
+                            //    }
+
+                            //    g.DrawString(text[i].ToString(), f, b, x, y);
+                            //}
+                            g.DrawString(text, f, b, x, y);
+                        }
+                    }
+                }
+                return new Bitmap(imgSrc);
+                //string fontpath = AppDomain.CurrentDomain.BaseDirectory + @"save.png";
+                //imgSrc.Save(fontpath, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("添加失败" + e.Message);
+                return null;
+            }
+        }
+
+    }
+}
